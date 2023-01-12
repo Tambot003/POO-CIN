8c3ce2a1828431a369d50f6665fb7e4f314047a7
Tameem <th468737@gmail.com>
2023-01-12 13:50:11 +0100

    first commit

---
+++ Personne.php
@@ -0,0 +1,12 @@
+<php?
+
+class Personne{
+    private string $nom;
+    private string $prenom;
+    private string $sex;
+    private string $dtnaissance;
+
+    public function __construct($nom, $prenom, sex, $dtnaissance){
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<php?
+$actuer1
+
+$genre1^
+
+$creator1
+
+$filme1
+
+$role1
+
+$casting1
